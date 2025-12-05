c0fb53f062b91425e40bfe445578abefed73ec31
Omer-uni <omerkltroff@gmail.com>
2025-12-05 04:47:09 +0300

    cargoproject

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+def login_view(request):
+    return render(request,"login.html")
+
+def register_view(request):
+    return render(request,"register.html")
